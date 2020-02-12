381a06b87875d59164e46d8fc5e17d93f90a0aa8
Schalk1985 <sschalkwillem@gmail.com>
2020-02-12 07:38:29 +0200

    Assigment Running Commit

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -21,10 +21,12 @@ public class Ingredient {
     public Ingredient() {
 
     }
-    public Ingredient(String description, BigDecimal Amount, UnitOfMeasure uom){
+    public Ingredient(String description, BigDecimal Amount, UnitOfMeasure uom, Recipe recipe){
         this.description = description;
         this.amount = amount;
         this.uom = uom;
+        this.recipe = recipe;
+
     }
 
     public Long getId() {
